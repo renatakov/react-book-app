0575cc3bf233442edc81b18a78e4b28d4c867cff
renatakov <renatakov1010@gmail.com>
2023-09-23 21:09:25 +0200

    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
+import React, { useEffect } from "react"; // Импорт React и useEffect
+import axios from "axios";
+
 const App = () => {
-  return(
+  // useEffect(() => {
+  //   async function getResponse() {
+  //     try {
+  //       const response = await axios.get("https://www.googleapis.com/books/v1/volumes/STmeDAAAQBAJ?key=AIzaSyDZW1GsI_95nbr63_i8k6P4B1l-PcMnVKQ");
+  //       console.log(response.data);
+  //     } catch (error) {
+  //       console.error("Произошла ошибка при выполнении запроса:", error);
+  //     }
+  //   }
+
+  //   getResponse(); // Вызываем функцию getResponse внутри useEffect
+
+  // }, []); // Пустой массив зависимостей, чтобы useEffect вызывался только при монтировании компонента
+
+  return (
     <>
-    <h1>Book App</h1>
+      <h1>Book App</h1>
     </>
-  )
+  );
 }
 
 export default App;
